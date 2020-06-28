4fce8c316e1e5fb10559687e60be4cfff416f36c
sparsh <spur-s@hotmail.com>
2020-06-29 00:18:44 +0545

    Registration

--- app/src/main/java/com/sparsh/vote/RegisterActivity.java
+++ app/src/main/java/com/sparsh/vote/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.sparsh.vote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -22,12 +24,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        txtname=(EditText)findViewById(R.id.txtname);
-        txtaddress=(EditText)findViewById(R.id.txtaddress);
-        txtphone=(EditText)findViewById(R.id.txtphone);
-        txtpassword=(EditText)findViewById(R.id.txtpassword);
-        txtage=(EditText)findViewById(R.id.txtage);
-        btnreg=(Button)findViewById(R.id.btnreg);
+        txtname=findViewById(R.id.txtname);
+        txtaddress=findViewById(R.id.txtaddress);
+        txtphone=findViewById(R.id.txtphone);
+        txtpassword=findViewById(R.id.txtpassword);
+        txtage=findViewById(R.id.txtage);
+        btnreg=findViewById(R.id.btnreg);
         member = new Member();
         reff= FirebaseDatabase.getInstance().getReference().child("Member");
         btnreg.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +37,26 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Integer agee=Integer.parseInt(txtage.getText().toString().trim());
                 Long phno=Long.parseLong(txtphone.getText().toString().trim());
+                if (TextUtils.isEmpty(txtname.getText().toString())) {
+                    txtname.setError("Required field");
+                    return;
+                }
+                if (TextUtils.isEmpty(txtphone.getText().toString())) {
+                    txtphone.setError("Required field");
+                    return;
+                }
+                if (TextUtils.isEmpty(txtaddress.getText().toString())) {
+                    txtaddress.setError("Required field");
+                    return;
+                }
+                if (TextUtils.isEmpty(txtpassword.getText().toString())) {
+                    txtpassword.setError("Required field");
+                    return;
+                }
+                if (TextUtils.isEmpty(txtage.getText().toString())) {
+                    txtage.setError("Required field");
+                    return;
+                }
 
                 member.setName(txtname.getText().toString().trim());
                 member.setAddress(txtaddress.getText().toString().trim());
@@ -44,6 +66,10 @@ public class RegisterActivity extends AppCompatActivity {
                 reff.push().setValue(member);
                 Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
             }
+
+
+
         });
     }
+
 }
