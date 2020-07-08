8614940967f20ed6f25530d0c8c6a0bfc674d9ce
madan-tiwari <rebelmadan13@gmail.com>
2020-07-08 21:54:12 +0545

    user profile design

--- app/src/main/java/com/sparsh/vote/navfragment/AccountFragment.java
+++ app/src/main/java/com/sparsh/vote/navfragment/AccountFragment.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sparsh.vote.R;
 
@@ -14,6 +18,10 @@ import com.sparsh.vote.R;
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment {
+    TextView tvfullname ;
+    Button btnsuscribe;
+    ImageView profilepicimageview, coverpicimageview;
+    RecyclerView recycler_viewpoll;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -24,6 +32,18 @@ public class AccountFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
+        tvfullname= view.findViewById(R.id.tvfullname);
+        btnsuscribe= view.findViewById(R.id.btnsuscribe);
+        profilepicimageview= view.findViewById(R.id.profilepicimageview);
+        coverpicimageview= view.findViewById(R.id.coverpicimageview);
+        recycler_viewpoll= view.findViewById(R.id.recycler_viewpoll);
+
+        btnsuscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
